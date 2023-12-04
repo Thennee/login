3918504b09d616ca98ba20e12479e1b09a1f404f
Thennee <101799630+Thennee@users.noreply.github.com>
2023-12-04 17:15:29 +0700

    Update index.html

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
-// Cek apakah tombol submit sudah ditekan atau belum
-if( isset($_POST["submit"]) ) {
-    // Cek Username & Password
-    if( $_POST["username"] == "admin" && $_POST["password"] == "321321" ) {
-    // Jika benar, redirect ke halaman admin
-        header("Location: admin.php");
-    }else{ 
-    // Else, tampilkan pesan kesalahan
-        $error = true;
 
-
+    if(!isset($_POST["username"])) {
+        header("Location: index.php");
     }
 
-}
 ?>
 
 <!DOCTYPE html>
@@ -21,31 +12,13 @@ if( isset($_POST["submit"]) ) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Halaman Admin</title>
 </head>
 <body>
     
-    <h1>Login Admin</h1>
+    <h1>Selamat Datang <?= $_POST["username"] ?></h1>
 
-    <?php  if(isset($error)) :?>
-        <p style="color: red; font-weight: bold;">Password / Username Yang Anda Masukkan Salah!</p>
-    <?php  endif; ?>
-        
-    <ul>
-        <form action="admin.php" method="post">
-            <li>
-                <label for="username">Username :</label>
-                <input type="text" id="username" name="username">
-            </li>
-            <li>
-                <label for="password">Password :</label>
-                <input type="password" id="password" name="password">
-            </li>
-            <li>
-                <button type="submit" name="submit">Login</button>
-            </li>
-        </form>
-    </ul>
-        
+    <a href="index.php">Logout</a>
+
 </body>
 </html>
